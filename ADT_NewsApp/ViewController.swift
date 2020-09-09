2b95e0c75beeff742eccfafec12c8a25583274f3
Amit Biswas <biswas1992@hotmail.com>
2020-09-08 23:08:10 -0400

    Initial Commit

---
+++ ADT_NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADT_NewsApp
+//
+//  Created by Amit Biswas on 9/8/20.
+//  Copyright © 2020 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
